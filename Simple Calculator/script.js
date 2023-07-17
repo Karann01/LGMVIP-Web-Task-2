9fee7ca904bf0d9a9d4aec5edf7591ca600f74e3
Karan Dhongade <89577456+Karann01@users.noreply.github.com>
2023-07-17 10:51:00 -0300

    Add files via upload

---
+++ Simple Calculator/script.js
@@ -0,0 +1,14 @@
+const calculate = () => {
+
+    const display = document.getElementById('display');
+
+    if (display.value == "") {
+
+    }
+    else {
+
+        display.value = eval(display.value);
+
+    }
+
+}
